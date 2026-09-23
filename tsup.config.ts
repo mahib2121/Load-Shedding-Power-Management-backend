9986537a11d1d0412b968bdeb460f2eefb8efcb9
mahib2121 <khanmahibalam@gmail.com>
2026-09-23 23:52:47 +0600

    Vercel & Tsup added

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/server.ts"],
+  format: ["esm"],
+  target: ["esnext"],
+  platform: "node",
+  outDir: "dist",
+  bundle: true,
+  banner: {
+    js: /* ts */ `
+   import { createRequire } from 'module';
+   const require = createRequire(import.meta.url);
+    `,
+  },
+});
